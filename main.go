70c87878970fc6ce076a1ac4e8bc233363d61106
Matt <19471665+mdloucks@users.noreply.github.com>
2024-12-27 18:02:57 -0500

    box updated

--- main.go
+++ main.go
@@ -134,15 +134,23 @@ func main() {
 	game := NewGame()
 
 	// Create the ground body
+	x, y, w, h := 100.0, 100.0, 300.0, 30.0
 	groundDef := box2d.MakeB2BodyDef()
-	groundDef.Position.Set(100, 100)
-	groundImage := ebiten.NewImage(300, 30)
+	groundDef.Position.Set(x, y)
+	groundImage := ebiten.NewImage(int(w), int(h))
 	groundImage.Fill(color.CMYK{100, 200, 30, 1})
 	groundBody := game.makeEntity("Ground", &groundDef, groundImage)
 	groundBody.SetType(box2d.B2BodyType.B2_staticBody)
 
-	groundShape := box2d.MakeB2EdgeShape()
-	groundShape.Set(box2d.MakeB2Vec2(0, 0), box2d.MakeB2Vec2(300, 0))
+	groundShape := box2d.MakeB2PolygonShape()
+	vertices := []box2d.B2Vec2{
+		box2d.MakeB2Vec2(0, 0), // bottom-left corner (relative to the body's position)
+		box2d.MakeB2Vec2(w, 0), // bottom-right corner (relative to the body's position)
+		box2d.MakeB2Vec2(w, h), // top-right corner (relative to the body's position)
+		box2d.MakeB2Vec2(0, h), // top-left corner (relative to the body's position)
+
+	}
+	groundShape.Set(vertices, len(vertices))
 	groundBody.CreateFixture(&groundShape, 0.0)
 
 	ebiten.SetWindowSize(screenWidth, screenHeight)
